cfb39e8428d4b679d74568b41f8ef95677e46369
Daniel Park <ekklesia89@gmail.com>
2019-09-30 15:06:48 +0900

    :tada: eslint prettier react-native-dependencies install

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  parser: 'babel-eslint',
+  env: {
+    jest: true
+  },
+  rules: {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off'
+  }
+};
