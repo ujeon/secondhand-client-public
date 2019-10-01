84f3cdac162bc10955c9d4dfb193326891c6c16d
Daniel Park <ekklesia89@gmail.com>
2019-10-01 16:09:43 +0900

    :art: Modify double qoute to single and boolean type ignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
-  extends: ["airbnb", "prettier", "prettier/react"],
-  parser: "babel-eslint",
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  parser: 'babel-eslint',
   env: {
     jest: true
   },
   rules: {
-    "no-use-before-define": "off",
-    "react/jsx-filename-extension": "off",
-    "react/prop-types": "off",
-    "import/no-unresolved": "off",
-    "react/prefer-stateless-function": "off"
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'import/no-unresolved': 'off',
+    'react/prefer-stateless-function': 'off',
+    'react/jsx-boolean-value': 'off'
   }
 };
