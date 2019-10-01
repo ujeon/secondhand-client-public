11b433df170308fc1e271825dce4e59ae0a6560b
KHwii <strider6k@gmail.com>
2019-10-01 14:31:42 +0900

    :sparkles: Add Mypage components and pages

---
+++ Bottom-tabs/Mypage/credit.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const credit = () => {
+  return (
+    <View>
+      <Text>만든 사람1</Text>
+      <Text>만든 사람2</Text>
+      <Text>만든 사람3</Text>
+      <Text>만든 사람4</Text>
+    </View>
+  );
+};
+
+export default credit;
