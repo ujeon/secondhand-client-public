cee7fae1eaad045b572c21a641705f3b4a11185e
Ujeon <wj9304@gmail.com>
2019-10-03 13:58:28 +0900

    :art: Modify result page

--- Bottom-tabs/Home/result.js
+++ Bottom-tabs/Home/result.js
@@ -1,57 +1,68 @@
 import React, { Component } from "react";
-// import { Text, View } from "react-native";
+import { Text, View, StyleSheet, ScrollView, Dimensions } from "react-native";
 import ProductList from "../components/productList";
+
+import { data as fakeData } from "../../fakeData";
+
+const { width } = Dimensions.get("window");
 
 export default class Result extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: {
-        average_price: {
-          id: 1,
-          brand: "STOKE",
-          model: "S9",
-          date: "2019-10-01",
-          average_price: 23000,
-          lowest_price: 20000,
-          highest_price: 40000,
-          quantity: 24
-        },
-        filtered_data: [
-          {
-            id: 1,
-            brand: "STOKE",
-            model: "S9",
-            price: 300000,
-            url: "https://www.hellomarket.com/item/163618711",
-            img_url:
-              "https://ccimg.hellomarket.com/images/2019/item/08/25/14/2308_4152473_1.jpg?size=s6",
-            market: "헬로마켓",
-            posted_at: null,
-            is_sold: false,
-            category_id: 1,
-            location: "-"
-          },
-          {
-            id: 2,
-            brand: "STOKE",
-            model: "S9",
-            price: 230000,
-            url: "https://www.hellomarket.com/item/163560910",
-            img_url:
-              "https://ccimg.hellomarket.com/images/2019/item/08/20/12/5312_4013953_1.jpg?size=s6",
-            market: "당근마켓",
-            posted_at: null,
-            is_sold: false,
-            category_id: 1,
-            location: "서울 서초구"
-          }
-        ]
-      }
+      data: fakeData
     };
   }
 
   render() {
-    return <ProductList data={this.state.data} />;
+    return (
+      // NOTE 종단 스크롤 뷰 안에 횡단 스크롤 뷰를 넣으면 가로, 세로 스크롤 모두 가능!
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>저의 제목을 지어주세요</Text>
+        </View>
+        <ScrollView
+          style={styles.container}
+          pagingEnabled={true}
+          horizontal={true}
+          decelerationRate={0}
+          snapToInterval={width - 60}
+          snapToAlignment="center"
+          contentInset={{
+            top: 0,
+            left: 30,
+            bottom: 0,
+            right: 30
+          }}
+          showsHorizontalScrollIndicator={false}
+        >
+          <View style={styles.view} />
+          <View style={styles.view} />
+          <View style={styles.view} />
+          <View style={styles.view} />
+          <View style={styles.view} />
+        </ScrollView>
+        <ProductList data={this.state.data} />
+      </ScrollView>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {},
+  titleContainer: {
+    paddingTop: "5%"
+  },
+  title: {
+    fontSize: 26
+  },
+  view: {
+    marginTop: "1.5%",
+    backgroundColor: "#2e2e2e",
+    width: width - 80,
+    margin: 10,
+    height: 300,
+    borderRadius: 10,
+    paddingHorizontal: 30
+  }
+});
