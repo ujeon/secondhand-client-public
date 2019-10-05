c4ed8d722023f27870a23935266873745c9470f8
KHwii <strider6k@gmail.com>
2019-10-05 21:56:48 +0900

    :art: Add ,

--- Bottom-tabs/Home/result.js
+++ Bottom-tabs/Home/result.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, ScrollView, Dimensions, AsyncStorage } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+  AsyncStorage
+} from "react-native";
 import ProductList from "../components/productList";
 import AverageMonthly from "./averageMonthly";
 
@@ -10,70 +17,74 @@ export default class Result extends Component {
     super(props);
     this.state = {
       data: null,
-      model: this.props.navigation.getParam("model")
+      model: this.props.navigation.getParam("model"),
       favoriteData: null
     };
   }
 
   async componentDidMount() {
-    const brand = this.props.navigation.getParam('brand')
-    const model = this.props.navigation.getParam('model')
-   
-    const data = await fetch(`http://3.17.152.1:8000/api/${brand}/${model}/info`)
-    .then(res => res.json())
-    .then(res => res)
-    .catch(err => console.error(err))
-    
-    const token = await AsyncStorage.getItem("token");
-    let favoriteData =[]
-    if(token) {
-    favoriteData = await fetch('http://3.17.152.1:8000/user/favorite/info/', {
-      headers: { token }
-    }).then(res => res.json())
-    .then(res => res)
-    .catch(err => console.error(err))
-    }
-    
-    const filteredData = data.filtered_data.map((el) => {
-      for(let i=0 ; i<favoriteData.length ; i++) {
-        if(favoriteData[i].id === el.id) {
-          el.isFavorite = true
-          break
-        } else {
-          el.isFavorite = false
-        }
-      } 
-      return el
-    })
-    data.filtered_data = filteredData
-    this.setState({
-      data, favoriteData
-    })
-  }
+    const brand = this.props.navigation.getParam("brand");
+    const model = this.props.navigation.getParam("model");
 
-  toggleFavorite = (id) => {
-    const clonedData = { ...this.state.data}
-    clonedData.filtered_data = clonedData.filtered_data.map((el) => {
-      if(el.id === id) {
-        if(el.isFavorite === true) {
-          el.isFavorite = false
+    const data = await fetch(
+      `http://3.17.152.1:8000/api/${brand}/${model}/info`
+    )
+      .then(res => res.json())
+      .then(res => res)
+      .catch(err => console.error(err));
+
+    const token = await AsyncStorage.getItem("token");
+    let favoriteData = [];
+    if (token) {
+      favoriteData = await fetch("http://3.17.152.1:8000/user/favorite/info/", {
+        headers: { token }
+      })
+        .then(res => res.json())
+        .then(res => res)
+        .catch(err => console.error(err));
+    }
+
+    const filteredData = data.filtered_data.map(el => {
+      for (let i = 0; i < favoriteData.length; i++) {
+        if (favoriteData[i].id === el.id) {
+          el.isFavorite = true;
+          break;
         } else {
-          el.isFavorite = true
+          el.isFavorite = false;
         }
       }
-      return el
-    })
-    const clonedFavoriteData = this.state.favoriteData.slice()
-    for(let i=0 ; i<clonedFavoriteData.length ; i++) {
-      if(clonedFavoriteData[i].id === id) {
-        clonedFavoriteData.splice(i, 1)
-      } 
+      return el;
+    });
+    data.filtered_data = filteredData;
+    this.setState({
+      data,
+      favoriteData
+    });
+  }
+
+  toggleFavorite = id => {
+    const clonedData = { ...this.state.data };
+    clonedData.filtered_data = clonedData.filtered_data.map(el => {
+      if (el.id === id) {
+        if (el.isFavorite === true) {
+          el.isFavorite = false;
+        } else {
+          el.isFavorite = true;
+        }
+      }
+      return el;
+    });
+    const clonedFavoriteData = this.state.favoriteData.slice();
+    for (let i = 0; i < clonedFavoriteData.length; i++) {
+      if (clonedFavoriteData[i].id === id) {
+        clonedFavoriteData.splice(i, 1);
+      }
     }
     this.setState({
-      data: clonedData, 
+      data: clonedData,
       favoriteData: clonedFavoriteData
-    })
-  }
+    });
+  };
 
   render() {
     if (this.state.data) {
@@ -97,7 +108,10 @@ export default class Result extends Component {
           >
             <AverageMonthly model={this.state.model} />
           </ScrollView>
-          <ProductList data={this.state.data} toggleFavorite={this.toggleFavorite}/>
+          <ProductList
+            data={this.state.data}
+            toggleFavorite={this.toggleFavorite}
+          />
         </ScrollView>
       );
     }
